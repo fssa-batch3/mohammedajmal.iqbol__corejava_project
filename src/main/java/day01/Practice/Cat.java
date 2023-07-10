db180fe0d20daf85c86982035a210c85f8e8a92b
Ajmal112 <118426017+Ajmal112@users.noreply.github.com>
2023-07-10 15:53:38 +0530

    initial commit

--- src/main/java/day01/Practice/Cat.java
+++ src/main/java/day01/Practice/Cat.java
@@ -24,13 +24,10 @@ public class Cat {
         this.color = color;
     }
     
-    public String getSpeaks() {
-        return speaks;
+    public static String getSpeaks() {
+        return "Meow!";
     }
 
-    public void setSpeaks(String speaks) {
-        this.speaks = speaks;
-    }
 
     // Main method to create Dog objects
     public static void main(String[] args) {
@@ -38,22 +35,19 @@ public class Cat {
     	Cat cat1 = new Cat();
     	cat1.setColor("purple");
     	cat1.setAge(4);
-    	cat1.setSpeaks("Meow!");
     	Cat cat2 = new Cat();
     	cat2.setColor("white");
     	cat2.setAge(3);
-    	cat2.setSpeaks("Meow!");
-
 
         // Print the attributes of each Dog
         System.out.println("Cat 1:");
         System.out.println("Age: " + cat1.getAge());
         System.out.println("Color: " + cat1.getColor());
-        System.out.println("Speaks: " + cat1.getSpeaks());
+        System.out.println("Speaks: " + getSpeaks());
 
         System.out.println("Cat 2:");
         System.out.println("Age: " + cat2.getAge());
         System.out.println("Color: " + cat2.getColor());
-        System.out.println("Speaks: " + cat2.getSpeaks());
+        System.out.println("Speaks: " + getSpeaks());
     }
 }
