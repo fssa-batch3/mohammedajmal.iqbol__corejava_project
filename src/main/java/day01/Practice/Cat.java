cf518fcf30ea39ebeeb7a926bf0f85a58123c29e
Ajmal112 <118426017+Ajmal112@users.noreply.github.com>
2023-07-10 14:54:59 +0530

    initial commit core java day01 solved practice

---
+++ src/main/java/day01/Practice/Cat.java
@@ -0,0 +1,59 @@
+package day01.Practice;
+
+public class Cat {
+    // Attributes
+    private String color;
+    private int age;
+    private String speaks;
+
+
+    // Getters and setters
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public String getSpeaks() {
+        return speaks;
+    }
+
+    public void setSpeaks(String speaks) {
+        this.speaks = speaks;
+    }
+
+    // Main method to create Dog objects
+    public static void main(String[] args) {
+        // Create two Dog objects
+    	Cat cat1 = new Cat();
+    	cat1.setColor("purple");
+    	cat1.setAge(4);
+    	cat1.setSpeaks("Meow!");
+    	Cat cat2 = new Cat();
+    	cat1.setColor("white");
+    	cat1.setAge(3);
+    	cat1.setSpeaks("Meow!");
+
+
+        // Print the attributes of each Dog
+        System.out.println("Cat 1:");
+        System.out.println("Age: " + cat1.getAge());
+        System.out.println("Color: " + cat1.getColor());
+        System.out.println("Speaks: " + cat1.getSpeaks());
+
+        System.out.println("Cat 2:");
+        System.out.println("Age: " + cat1.getAge());
+        System.out.println("Color: " + cat1.getColor());
+        System.out.println("Speaks: " + cat1.getSpeaks());
+    }
+}
---
+++ src/main/java/day01/Practice/practice.java
@@ -0,0 +1,5 @@
+package day01.Practice;
+
+public class practice {
+
+}
