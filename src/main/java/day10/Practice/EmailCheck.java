38d43e9b671300bb5d7b0e11f90fb86fefe7eda2
Ajmal112 <118426017+Ajmal112@users.noreply.github.com>
2023-07-23 11:03:30 +0530

    initial commit corejava day10

--- src/main/java/day10/Practice/EmailCheck.java
+++ src/main/java/day10/Practice/EmailCheck.java
@@ -1,15 +1,46 @@
 package day10.Practice;
 
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 import day10.Solved.ValidationException;
 
-public class EmailCheck {
+class Email {
+	String email;
+	
+	public Email(String email) {
+		this.email = email;
+	}
+}
 
+class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
+
+class EmailValidate {
+	
+	public void validate(Email e)  throws InvalidEmailException {
+		 String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+			if(Pattern.matches(emailRegex , e.email)) {
+				System.out.println("The Given " + e.email + " Email Is Valid");
+				return;
+			}
+		throw new InvalidEmailException( "The Given " + e.email + " Email Is Invalid");
+	}
+}
+
+
+public class EmailCheck {
 	public static void main(String[] args) {
 		
+		EmailValidate em = new EmailValidate();
 		try {
-			EmailValidator.validate();
-		} catch (ValidationException e) {
-			e.printStackTrace();
+            Email e = new Email("freekyajmal@example.com");
+			em.validate(e);
+		} catch (InvalidEmailException e) {
+			System.err.println("Error: " + e.getMessage());
 		}
 	}
 }
