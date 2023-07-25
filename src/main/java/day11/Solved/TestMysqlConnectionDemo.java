a7a4004b98ca9a892b907d02e6d1e58da8b14f1a
Ajmal112 <118426017+Ajmal112@users.noreply.github.com>
2023-07-25 15:56:20 +0530

    initial commit corejava day12 added

--- src/main/java/day11/Solved/TestMysqlConnectionDemo.java
+++ src/main/java/day11/Solved/TestMysqlConnectionDemo.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * @author Mohammed Ajmal
+ * @author MohammedAjmal
  *
  */
 public class TestMysqlConnectionDemo {
@@ -16,9 +16,7 @@ public class TestMysqlConnectionDemo {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection conn = DriverManager.
-														getConnection("jdbc:mysql://localhost:3306/project", 
-																					"root", 
-																	"root");
+        getConnection("jdbc:mysql://localhost:3306/project", "root", "root");
 		System.out.println(conn);
 
 	}
---
+++ src/main/java/day12/Solved/UserQuery.java
@@ -0,0 +1,54 @@
+package day12.Solved;
+
+import day11.Solved.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+ 
+/**
+ * @author MohammedAjmal
+ *
+ */
+
+public class UserQuery {
+ 
+    public static void main(String[] args) throws Exception {       
+         
+        
+        Connection connection = ConnectionUtil.getConnection();
+        System.out.println(connection);
+         
+        Statement stmt = connection.createStatement();
+         
+        // Step 03: Execute Insert Query
+        String query ="INSERT INTO user (username, email, password) VALUES (\"vinit_gore\",\"vinit.gore@ctr.freshworks.com\", \"password007\")";
+        int rows = stmt.executeUpdate(query);
+        System.out.println("No of rows inserted :" + rows );
+         
+        //Step 04: Execute SELECT Query
+        final String selectQuery = "SELECT username,email FROM USER";
+         
+        //Step 05: Get the resultset
+        ResultSet rs = stmt.executeQuery(selectQuery);
+        
+        //Step 06: Iterate the result
+        while ( rs.next()) {
+        	           
+        	String userName = rs.getString("username");
+            String emailID = rs.getString("email");
+             
+            System.out.println("UserId:" + userId +", UserName:" + userName + ", EMAIL ID:" + emailID);
+        }
+         
+        //Step 07: close the connection resources       
+        rs.close();
+        stmt.close();
+        connection.close();
+         
+         
+         
+    }
+     
+}
+```
